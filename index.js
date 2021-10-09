47d76d83173aad52dce3059e23d9ff5ec365a174
Tuomas Kemppainen <tuomas.kemppainen@student.hamk.fi>
2021-10-10 00:41:13 +0300

    complete part 3.4.

--- index.js
+++ index.js
@@ -40,7 +40,14 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    contacts = contacts.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
